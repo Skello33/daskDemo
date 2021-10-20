3570ef81df1dfa3b06ca0831d6fce5119182474b
Skello33 <smadot@gmail.com>
2021-10-20 16:06:02 +0200

    small update on compute function

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ def demo_ops_dask(df: dd.DataFrame) -> dd.DataFrame:
     dep_del_mean = df['DepDelay'].mean()
     arr_del_mean = df['ArrDelay'].mean()
     mean_delay = df.groupby('Origin')['DepDelay'].mean()
+    # dep_del_mean, arr_del_mean, mean_delay = compute(dep_del_mean, arr_del_mean, mean_delay)
+
     dep_del_mean, arr_del_mean = compute(dep_del_mean, arr_del_mean)
     mean_delay, = compute(mean_delay)
 
@@ -73,7 +75,7 @@ def pandas_more_files():
 
 def dask_main():
     # start_time = ti.default_timer()
-    files = os.path.join('data', 'DataExpo2009', '*.csv')
+    files = os.path.join('data', 'DataExpo2009', '2008.csv')
     # print("dask files: {}".format(files))
     df = dd.read_csv(files, dtype={'ActualElapsedTime': 'float64',
                                    'ArrDelay': 'float64',
@@ -99,7 +101,7 @@ def dask_main():
 
 def pandas_main():
     # start_time = ti.default_timer()
-    files = sorted(glob(os.path.join('data', 'DataExpo2009', '*.csv')))
+    files = sorted(glob(os.path.join('data', 'DataExpo2009', '2008.csv')))
     total_len = 0
     for file in files:
         temp_df = dd.read_csv(file, assume_missing=True)
@@ -117,13 +119,13 @@ if __name__ == '__main__':
     #     xtime=ti.timeit(stmt=pandas_more_files, number=runs, setup="gc.enable()")))
 
     # create a local cluster
-    # client = Client(n_workers=4)
+    client = Client(n_workers=4)
 
     # open the cluster dashboard in browser
     # webbrowser.open(client.dashboard_link, new=2, autoraise=True)
 
     # connect to a cluster
-    client = Client('tcp://10.0.2.15:8786')
+    # client = Client('tcp://10.0.2.15:8786')
     print(client.dashboard_link)
 
     print("Dask finished in {xtime} seconds.".format(
