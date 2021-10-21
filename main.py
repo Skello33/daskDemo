d069e09b9463a9882cb78a8e262cf89df341205a
Skello33 <smadot@gmail.com>
2021-10-21 09:33:02 +0200

    small update on compute function

--- main.py
+++ main.py
@@ -14,31 +14,28 @@ import time
 
 def demo_ops_dask(df: dd.DataFrame) -> dd.DataFrame:
     dep_del_mean = df['DepDelay'].mean()
-    arr_del_mean = df['ArrDelay'].mean()
+    # arr_del_mean = df['ArrDelay'].mean()
     mean_delay = df.groupby('Origin')['DepDelay'].mean()
-    dep_del_mean, arr_del_mean = compute(dep_del_mean, arr_del_mean)
+    # dep_del_mean, arr_del_mean = compute(dep_del_mean, arr_del_mean)
     mean_delay, = compute(mean_delay)
+    dep_del_mean, = compute(dep_del_mean)
 
-    print("Mean dep delay for ABE is {}".format(mean_delay['ABE']))
+    print("Avg dep delay for ABE is {}".format(mean_delay['ABE']))
     print("Avg dep delay is {}".format(dep_del_mean))
-    print("Avg arr delay is {}".format(arr_del_mean))
+    # print("Avg arr delay is {}".format(arr_del_mean))
     return df
 
 
 def demo_ops_pandas(df: pd.DataFrame) -> list:
     dep_group = df.groupby('Origin')
-    delay_cnt = dep_group['DepDelay'].count()['ABE']
-    delay_sum = dep_group['DepDelay'].sum()['ABE']
-    # print(delay_sum / delay_cnt)
-    # abe_delay = dep_group['DepDelay'].mean()['ABE']
-    # print(abe_delay)
+    abe_delay_cnt = dep_group['DepDelay'].count()['ABE']
+    abe_delay_sum = dep_group['DepDelay'].sum()['ABE']
+    dep_delay_cnt = df['DepDelay'].count()
+    dep_delay_sum = df['DepDelay'].sum()
 
-    # print(mean_delay['ABE'])
-    dep_mean = df['DepDelay'].mean()
-    arr_mean = df['ArrDelay'].mean()
     # print(df['DepDelay'].mean())
     # print(df['ArrDelay'].mean())
-    return [delay_cnt, delay_sum]
+    return [abe_delay_cnt, abe_delay_sum, dep_delay_cnt, dep_delay_sum]
 
 
 def pandas_task():
@@ -56,18 +53,16 @@ def pandas_more_files():
     files = glob(os.path.join('data', 'DataExpo2009', '*.csv'))
     # print("pandas files: {}".format(files))
     file_count = len(files)
-    del_cnt, del_sum = [], []
-    # abe_li, dep_li, arr_li = [], [], []
+    abe_cnt, abe_sum, del_cnt, del_sum = [], [], [], []
     for file in files:
         res = pd.read_csv(file)
         res = demo_ops_pandas(res)
-        del_cnt.append(res[0])
-        del_sum.append(res[1])
-        # abe_li.append(res[0])
-        # dep_li.append(res[1])
-        # arr_li.append(res[2])
-    # print("Mean departure delay for ABE is {}".format(sum(del_sum) / sum(del_cnt)))
-
+        abe_cnt.append(res[0])
+        abe_sum.append(res[1])
+        del_cnt.append(res[2])
+        del_sum.append(res[3])
+    print("Average departure delay for ABE is {}".format(sum(abe_sum) / sum(abe_cnt)))
+    print("Average departure delay is {}".format(sum(del_sum) / sum(del_cnt)))
     # print("Pandas task took {} seconds.".format(ti.default_timer() - start_time))
 
 
@@ -83,7 +78,10 @@ def dask_main():
                                    'Distance': 'float64',
                                    'CRSElapsedTime': 'float64',
                                    'CancellationCode': 'object',
-                                   'TailNum': 'object'
+                                   'TailNum': 'object',
+                                   'AirTime': 'float64',
+                                   'TaxiIn': 'float64',
+                                   'TaxiOut': 'float64'
                                    })
 
     # print(df['CRSElapsedTime'].mean().compute())
@@ -110,8 +108,8 @@ def pandas_main():
 if __name__ == '__main__':
     # execute the tasks
     runs = 1
-    # print("Pandas finished in {xtime} seconds.".format(
-    #     xtime=ti.timeit(stmt=pandas_more_files, number=runs, setup="gc.enable()")))
+    print("Pandas finished in {xtime} seconds.".format(
+        xtime=ti.timeit(stmt=pandas_more_files, number=runs, setup="gc.enable()")))
 
     # create a local cluster
     # client = Client(n_workers=4)
@@ -119,12 +117,12 @@ if __name__ == '__main__':
     # open the cluster dashboard in browser
     # webbrowser.open(client.dashboard_link, new=2, autoraise=True)
 
-    # connect to a cluster
+    # connect to a remote cluster
     client = Client('tcp://10.0.2.15:8786')
     print(client.dashboard_link)
 
     print("Dask finished in {xtime} seconds.".format(
-        xtime=ti.timeit(stmt=dask_main, number=runs)))
+        xtime=ti.timeit(stmt=dask_main, number=runs, setup="gc.enable()")))
 
     # pandas_task()
 
