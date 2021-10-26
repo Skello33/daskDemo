a589c7e56f803da9539261eece6f085ee2820b0b
Skello33 <smadot@gmail.com>
2021-10-26 15:13:33 +0200

    reformating and cleaning the file, update on client handling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import timeit as ti
 import webbrowser
-
+from datetime import datetime
 import dask
 import dask.dataframe as dd
 from dask.distributed import Client
@@ -9,7 +9,21 @@ from dask import compute
 import pandas as pd
 import os
 from glob import glob
-import time
+
+
+def reformat_time(time: str) -> datetime.time:
+    if time == '2400':
+        time = '2359'
+    if len(time) != 4:
+        time = ''.join(('000', time))
+        time = time[-4:]
+    # try:
+    #     result = datetime.strptime(time, '%H%M').time()
+    # except ValueError:
+    #     print(time)
+    #     exit()
+    # print(time)
+    return datetime.strptime(time, '%H%M').time()
 
 
 def demo_ops_dask(df: dd.DataFrame) -> dd.DataFrame:
@@ -20,16 +34,20 @@ def demo_ops_dask(df: dd.DataFrame) -> dd.DataFrame:
     arr_del_mean = df['ArrDelay'].mean()
     # avg arrival delays by Origin of flight
     mean_delay = df.groupby('Origin')['DepDelay'].mean()
+    # the day with lowest avg delay
+    day_delay = df.groupby('DayOfWeek')['DepDelay'].mean()
 
     # compute the results in parallel, might need some modification based on the size of data and machines memory
-    mean_delay, dep_del_mean, arr_del_mean = compute(mean_delay, dep_del_mean, arr_del_mean)
+    mean_delay, dep_del_mean, day_delay = compute(mean_delay, dep_del_mean, day_delay)
+    # day_delay,  = compute(day_delay)
     # dep_del_mean, = compute(dep_del_mean)
     # dep_del_mean, arr_del_mean = compute(dep_del_mean, arr_del_mean)
 
     # control prints
     print("Avg dep delay for ABE is {}".format(mean_delay['ABE']))
     print("Avg dep delay is {}".format(dep_del_mean))
-    print("Avg arr delay is {}".format(arr_del_mean))
+    # print("Avg arr delay is {}".format(arr_del_mean))
+    print("Avg day delays:\n{}".format(day_delay.sort_values()))
     return df
 
 
@@ -42,8 +60,9 @@ def demo_ops_pandas(df: pd.DataFrame) -> list:
     # data for avg departure delay
     dep_delay_cnt = df['DepDelay'].count()
     dep_delay_sum = df['DepDelay'].sum()
+    day_group = df.groupby('DayOfWeek')['DepDelay'].mean()
 
-    return [abe_delay_cnt, abe_delay_sum, dep_delay_cnt, dep_delay_sum]
+    return [abe_delay_cnt, abe_delay_sum, dep_delay_cnt, dep_delay_sum, day_group]
     # return [abe_delay_cnt, abe_delay_sum]
 
 
@@ -59,34 +78,42 @@ def pandas_task():
 def pandas_more_files():
     # start_time = ti.default_timer()
 
-    files = glob(os.path.join('data', 'DataExpo2009', '2008.csv'))
+    files = glob(os.path.join('data', 'DataExpo2009', 'years_to_compute', '*.csv'))
     # print("pandas files: {}".format(files))
     # file_count = len(files)
-    cols = ['Year', 'Month', 'DayofMonth', 'DayOfWeek', 'DepDelay', 'CRSDepTime', 'DepTime', 'Origin', 'Dest',
+    cols = ['Year', 'Month', 'DayofMonth', 'DayOfWeek', 'DepDelay', 'CRSDepTime', 'Origin', 'Dest',
             'ArrDelay']
-    abe_cnt, abe_sum, del_cnt, del_sum = [], [], [], []
+    types_dict = {'CancellationCode': 'object',
+                  'CRSDepTime': 'string',
+                  'Year': 'string',
+                  'Month': 'string',
+                  'DayofMonth': 'string'}
+    abe_cnt, abe_sum, del_cnt, del_sum, day_delay = [], [], [], [], []
     for file in files:
         print("reading file {}".format(file))
 
-        res = pd.read_csv(file, parse_dates=[[0, 1, 2]], encoding_errors='replace', usecols=cols,
-                          dtype={'CancellationCode': 'object'})
+        res = pd.read_csv(file, encoding_errors='replace', usecols=cols,
+                          dtype=types_dict)
         res = demo_ops_pandas(res)
 
         abe_cnt.append(res[0])
         abe_sum.append(res[1])
-        if len(res) == 4:
+        if len(res) > 2:
             del_cnt.append(res[2])
             del_sum.append(res[3])
+            day_delay.append(res[4])
     print("Average departure delay for ABE is {}".format(sum(abe_sum) / sum(abe_cnt)))
     if len(del_cnt):
         print("Average departure delay is {}".format(sum(del_sum) / sum(del_cnt)))
+    if len(day_delay):
+        print("Avg day delays:\n{}".format(day_delay))
     # print("Pandas task took {} seconds.".format(ti.default_timer() - start_time))
 
 
 def dask_main():
     # start_time = ti.default_timer()
-    files = os.path.join('data', 'DataExpo2009', '2008.csv')
-    cols = ['Year', 'Month', 'DayofMonth', 'DayOfWeek', 'DepDelay', 'CRSDepTime', 'DepTime', 'Origin', 'Dest',
+    files = os.path.join('data', 'DataExpo2009', 'years_to_compute', '*.csv')
+    cols = ['Year', 'Month', 'DayofMonth', 'Month', 'DayOfWeek', 'DepDelay', 'CRSDepTime', 'Origin', 'Dest',
             'ArrDelay']
     col_types = {'ActualElapsedTime': 'float64',
                  'ArrDelay': 'float64',
@@ -99,13 +126,26 @@ def dask_main():
                  'TailNum': 'object',
                  'AirTime': 'float64',
                  'TaxiIn': 'float64',
-                 'TaxiOut': 'float64'
+                 'TaxiOut': 'float64',
+                 'CRSDepTime': 'string'
                  }
-    df = dd.read_csv(files, parse_dates=[[0, 1, 2]], encoding_errors='replace', dtype=col_types, usecols=cols)
+    # df = dd.read_csv(files, encoding_errors='replace', parse_dates=[[0, 1, 2]], dtype=col_types, usecols=cols,
+    #                  infer_datetime_format=True)
+    # df = df.rename(columns={'Year_Month_DayofMonth': 'Date'})
+
+    df = dd.read_csv(files, encoding_errors='replace', dtype=col_types, usecols=cols)
+    df['CRSDepTime'] = df['CRSDepTime'].apply(reformat_time, meta='datetime.time')
+    # print(df['CRSDepTime'].head())
+
+    # df['Date'] = (pd.to_datetime(df['Year'].astype(str) + '-' +
+    #                              df['Month'].astype(str) + '-' +
+    #                              df['DayofMonth'].astype(str) + ' ' +
+    #                              df['CRSDepTime'].apply(reformat_time, meta=('CRSDepTime', 'string'))
+    #                              )
+    #               )
 
     # call demo functions on the dataframe
     demo_ops_dask(df)
-
     # print("Dask took {} seconds.".format(ti.default_timer() - start_time))
 
 
@@ -123,6 +163,7 @@ def pandas_main():
 
 
 if __name__ == '__main__':
+
     # execute the tasks
     runs = 1
     print("Pandas finished in {xtime} seconds.".format(
